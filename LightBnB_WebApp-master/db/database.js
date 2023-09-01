e7dc035fa25eabaac4c7edde92218f8dd2a43c9e
Sunny Hundal <amandip.h@gmail.com>
2023-08-31 22:16:32 -0700

    Added getAllProperties + getUserById

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -11,7 +11,7 @@ const pool = new Pool ({
   database: 'lightbnb'
 })
 
-pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
+// pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
 
 
 /// Users
@@ -21,16 +21,34 @@ pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.l
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+// const getUserWithEmail = function (email) {
+//   let resolvedUser = null;
+//   for (const userId in users) {
+//     const user = users[userId];
+//     if (user && user.email.toLowerCase() === email.toLowerCase()) {
+//       resolvedUser = user;
+//     }
+//   }
+//   return Promise.resolve(resolvedUser);
+// };
+
+const getUserWithEmail =  (email) => {
+  return pool
+    .query(`SELECT * FROM users WHERE email like $1;`, [email])
+    .then((result) => {
+      console.log(result.rows);
+      if (result.rows.length === 0) {
+        return null;
+      } else {
+        return result.rows[0];
+      }
+      
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+
 
 /**
  * Get a single user from the database given their id.
@@ -72,12 +90,16 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1;`, [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
