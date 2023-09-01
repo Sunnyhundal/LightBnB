35119b4d59969c5855c2ee09b9984d26e29dbc63
Sunny Hundal <amandip.h@gmail.com>
2023-08-31 22:19:53 -0700

    add getUserWithId

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -56,7 +56,20 @@ const getUserWithEmail =  (email) => {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1;`, [id])
+    .then((result) => {
+      console.log(result.rows);
+      if (result.rows.length === 0) {
+        return null;
+      } else {
+        return result.rows[0];
+      }
+      
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
